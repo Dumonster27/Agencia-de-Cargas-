c940c763eb2e0f91adbc7afedfce7d4837e3d011
Eduardo Pereira Araujo <166282869+Dumonster27@users.noreply.github.com>
2026-02-17 18:34:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function enviarWhatsApp() {
+    
+    const meuNumero = "5514981457617"; 
+    
+    // Captura os valores dos campos
+    const nome = document.getElementById('nome').value;
+    const whats = document.getElementById('whatsapp').value;
+    const doc = document.getElementById('documento').value;
+    const veiculo = document.getElementById('veiculo').value;
+    const frutas = document.getElementById('experiencia').value;
+    const verduras = document.getElementById('verduras').value;
+
+    
+    if (nome === "" || whats === "") {
+        alert("Por favor, preencha o nome e o WhatsApp pelo menos.");
+        return;
+    }
+
+    
+    const mensagem = `🦅 *NOVO CADASTRO - ÁGUIA DOURADA* %0A%0A` +
+                     `👤 *Motorista:* ${nome}%0A` +
+                     `📱 *WhatsApp:* ${whats}%0A` +
+                     `📄 *Doc/ANTT:* ${doc}%0A` +
+                     `🚚 *Veículo:* ${veiculo}%0A` +
+                     `🍎 *Frutas:* ${frutas}`;
+                     `🥦 *Verduras:* ${verduras}`;
+
+    
+    const url = `https://api.whatsapp.com/send?phone=${meuNumero}&text=${mensagem}`;
+    
+    window.open(url, '_blank');
+}
